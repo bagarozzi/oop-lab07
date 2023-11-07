7b99b681d34aae587bfe284ed237d6a24ad905b1
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2023-11-07 14:58:52 +0100

    Created new class

---
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -0,0 +1,8 @@
+package it.unibo.inner.impl;
+
+import it.unibo.inner.api.IterableWithPolicy;
+import java.lang.Iterable;
+
+public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
+    
+}
