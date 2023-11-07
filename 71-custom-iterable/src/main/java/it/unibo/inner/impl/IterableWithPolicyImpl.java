4996991b241c83ae2fca0c3c9b4261934c8c704a
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2023-11-07 15:30:16 +0100

    Changes in Iterable class

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -1,8 +1,33 @@
 package it.unibo.inner.impl;
 
 import it.unibo.inner.api.IterableWithPolicy;
-import java.lang.Iterable;
+import it.unibo.inner.api.Predicate;
+import java.util.Iterator;
 
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
-    
+
+    public IterableWithPolicyImpl(){
+        
+    }
+
+    public void setIterationPolicy(Predicate<T> filter){}
+
+    public IteratorImpl iterator(){
+        IteratorImpl it = this.new IteratorImpl();
+        // it.index = this.?;
+        return it;
+    }
+
+    class IteratorImpl implements Iterator<T>{
+        private int index;
+        private int maxSize;
+
+        public boolean hasNext(){
+            return false;
+        }
+        
+        public T next(){
+            
+        }
+    }
 }
